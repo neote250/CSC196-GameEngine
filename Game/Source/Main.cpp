375d660778072cf03b164480bef4bcb07a4b6db1
neote250 <nornano@student.neumont.edu>
2024-06-28 10:18:50 -0600

    Add project files.

---
+++ Game/Source/Main.cpp
@@ -0,0 +1,44 @@
+#include <SDL.h>
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	// create window
+	// returns pointer to window if successful or nullptr if failed
+	SDL_Window* window = SDL_CreateWindow("Game Engine",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		800, 600,
+		SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		std::cerr << "Error creating SDL window: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	// create renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	while (true)
+	{
+		// clear screen
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderClear(renderer);
+
+		// draw line
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+		SDL_RenderDrawLine(renderer, 0, 0, 800, 600);
+
+		// show screen
+		SDL_RenderPresent(renderer);
+	}
+
+	return 0;
+}
